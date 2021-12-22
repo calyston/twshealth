91bf30fce4fc964fe10740e20a5cc2af3d6b4a95
Christine <calyston.14@gmail.com>
2021-12-22 18:24:21 +0000

    style: footer styled

--- footer.php
+++ footer.php
@@ -3,13 +3,25 @@
 </main>
 
 <footer>
-  <section class="sns">
-  <a href="https://www.instagram.com/tws.health/" target="blank"><i class="fab fa-instagram fa-4x"></i></a>
-  <a href="https://www.facebook.com/groups/tws.health/" target="blank"><i class="fab fa-facebook-f fa-4x"></i></a>
-  <a href="https://twitter.com/TWS_Health" target="blank"><i class="fab fa-twitter fa-4x"></i></a>
-  <a href="https://www.tiktok.com/@tws.health/" target="blank"><i class="fab fa-tiktok fa-4x"></i></a>
+  <section class="footer-content">
+    <a href="https://tws.health"><img id="footer-logo" src="https://tws.health/wp-content/uploads/2021/12/logo-3-white.png" alt="logo"></a>
+    <section class="footer-list">
+      <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+      <div>
+        <p>Phone: 07XXX-XXX-XXX</p>
+        <p>Email: xxx@tws.health</p>
+      </div>
+    </section>
+    <section class="sns">
+    <a href="https://www.instagram.com/tws.health/" target="blank"><i class="fab fa-instagram fa-4x"></i></a>
+    <a href="https://www.facebook.com/groups/tws.health/" target="blank"><i class="fab fa-facebook-f fa-4x"></i></a>
+    <a href="https://twitter.com/TWS_Health" target="blank"><i class="fab fa-twitter fa-4x"></i></a>
+    <a href="https://www.tiktok.com/@tws.health/" target="blank"><i class="fab fa-tiktok fa-4x"></i></a>
+    </section>
+    <p><a>Terms & Conditions</a> | <a>Privacy Policy</a></p>
+    <p>© TWS Health & Wellness 2021</p>
   </section>
-  <p>© TWS Health & Wellness 2021</p>
+
 </footer>
 
 <?php wp_footer(); ?>
