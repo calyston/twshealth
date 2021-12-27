b75b5e2153cc5ea593eef06c251f31e9961feb77
Christine <calyston.14@gmail.com>
2021-12-27 13:29:09 +0000

    style: testimonials page created and styled

--- page-services.php
+++ page-services.php
@@ -21,6 +21,56 @@ get_header();?>
     <?php the_content(); ?>
   </section>
 
+  <section class="pricing-content">
+    <div>
+      <h1>Pricing</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <section class="product-boxes">
+      <div class="product-box">
+        <h3>Product Name</h3>
+        <h1>£100</h1>
+        <p>per month</p>
+        <ul>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>Feature</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>Feature</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>Feature</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>Feature</li>
+        </ul>
+        <button class="main-button">Book A Call</button>
+      </div>
+
+      <div class="product-box highlighted-product">
+        <h3>Product Name</h3>
+        <h1>£100</h1>
+        <p>per month</p>
+        <ul>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>Lorem ipsum dolor sit amet</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>consectetur adipiscing elit</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>sed do eiusmod tempor incididunt ut</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>labore et dolore magna aliqua</li>
+        </ul>
+        <button class="main-button">Book A Call</button>
+      </div>
+
+      <div class="product-box">
+        <h3>Product Name</h3>
+        <h1>£100</h1>
+        <p>per month</p>
+        <ul>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>Feature</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>Feature</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>Feature</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>Feature</li>
+        </ul>
+        <button class="main-button">Book A Call</button>
+      </div>
+
+    </section>
+
+  </section>
+
 	<?php endwhile; ?>
 
 	<?php else : ?>
