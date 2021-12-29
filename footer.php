57191b13669a820fc52e48e90fa8d5a068e778b7
Christine <calyston.14@gmail.com>
2021-12-29 14:49:38 +0000

    feat: contact page created

--- footer.php
+++ footer.php
@@ -20,10 +20,10 @@
       </div>
     </section>-->
     <section class="sns">
-    <a href="https://www.instagram.com/tws.health/" target="blank"><i class="fab fa-instagram fa-4x"></i></a>
-    <a href="https://www.facebook.com/groups/tws.health/" target="blank"><i class="fab fa-facebook-f fa-4x"></i></a>
-    <a href="https://twitter.com/TWS_Health" target="blank"><i class="fab fa-twitter fa-4x"></i></a>
-    <a href="https://www.tiktok.com/@tws.health/" target="blank"><i class="fab fa-tiktok fa-4x"></i></a>
+      <a href="https://www.instagram.com/tws.health/" target="blank"><i class="fab fa-instagram fa-4x"></i></a>
+      <a href="https://www.facebook.com/groups/tws.health/" target="blank"><i class="fab fa-facebook-f fa-4x"></i></a>
+      <a href="https://twitter.com/TWS_Health" target="blank"><i class="fab fa-twitter fa-4x"></i></a>
+      <a href="https://www.tiktok.com/@tws.health/" target="blank"><i class="fab fa-tiktok fa-4x"></i></a>
     </section>
     <p><a>Terms & Conditions</a> | <a>Privacy Policy</a></p>
     <p>© TWS Health & Wellness 2021</p>
